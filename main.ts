cc13b4bad51c9509517e2093cda0f61a8b5d9d23
jmatcas997 <98690552+jmatcas997@users.noreply.github.com>
2022-02-07 20:11:53 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ basic.forever(function () {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 40)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 40)
     }
-    if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 40)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 40)
     }
