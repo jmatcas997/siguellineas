7c4fc33af5df21184f3da850b723f57c7caee383
jmatcas997 <98690552+jmatcas997@users.noreply.github.com>
2022-02-07 20:10:54 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 40)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 40)
+    }
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 40)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 40)
+    }
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 40)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 40)
+    }
+})
